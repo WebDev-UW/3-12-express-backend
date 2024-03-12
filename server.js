16637ca7ed07691e95d1e505abe5af66bfd4bef4
kavakian <38766915+kegdotcom@users.noreply.github.com>
2024-03-11 19:39:02 -0500

    changed hint

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // TODO: install express, import express, and create a new express app
-
-// hint: remember to define a port number
-// things can get very hard to debug if you mistype it later
+// hint: remember to tell express what middleware to use
 
 // TODO: define a GET request handler for the r
 
