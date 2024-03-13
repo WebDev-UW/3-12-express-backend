ea60c676cce6c6529e240675b60a082817c01585
nithilan4 <43860191+nithilan4@users.noreply.github.com>
2024-03-12 20:59:53 -0500

    Week 1 Progress

--- server.js
+++ server.js
@@ -1,4 +1,44 @@
 // TODO: install express, import express, and create a new express app
+const express = require("express")
+const app = express()
+const port = 3000
+const path = require('path');
+
+const posts = {
+
+}
+
+let nextPostId = 0
+
+app.use(express.json())
+
+app.get("/posts", (req, res) => {
+	res.json(posts)
+})
+
+app.post("/posts", (req, res) => {
+	const postId = nextPostId
+	nextPostId++
+
+	posts[postId] = {
+		id: postId,
+		name: req.body.name,
+		content: req.body.content,
+		timestamp: Date.now(),
+		comments: []
+	}
+
+	console.log("Added a post by " + req.body.name)
+
+	res.json({
+		postId: postId
+	})
+})
+
+app.listen(port, () => {
+	console.log("Express running on port " + port)
+})
+
 // hint: remember to tell express what middleware to use
 
 // TODO: define a GET request handler for the r
