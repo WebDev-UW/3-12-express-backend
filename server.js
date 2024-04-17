d75e07b02592810b69c364d6145fa26fd083154a
kavakian <38766915+kegdotcom@users.noreply.github.com>
2024-04-16 20:37:57 -0500

    post meeting on 4/16

--- server.js
+++ server.js
@@ -1,42 +1,53 @@
 // Import express and create a new express app listening on port 3000
-import express from "express"
-import db from "./connect.js"
-const app = express()
-const port = 3000
+import express from "express";
+import db from "./connect.js";
+const app = express();
+const port = 3000;
 
 // Handle incoming JSON data in POST requests
-app.use(express.json())
+app.use(express.json());
 
 // This endpoint handles GET requests to /posts and returns the last 50 posts sorted by timestamp descending
-app.get("/posts", async (req, res) => {
-	let collection = await db.collection("posts");
+//("http://localhost:3000/posts/12345/comments/ME");
+app.get("/posts/:author/top/:n", async (req, res) => {
+  let collection = await db.collection("posts");
+  let authorName = req.params.author;
+  let n = Number(req.params.n);
+  let posts = await collection.find({name: authorName}).toArray();
 
-	let documents = await collection.find().limit(50).sort({
-		timestamp: -1
-	}).toArray();	
+  posts.sort((post) => -post.likes);
+  let topn = posts.slice(0, n);
 
-	res.json(documents)
-})
+  res.json(topn);
+});
 
 // This endpoint handles POST requests to /posts and inserts a new post from the client into the database
 app.post("/posts", async (req, res) => {
-	// Define our post object
-	let newPost = {
-		name: req.body.name,
-		content: req.body.content,
-		timestamp: Date.now(),
-		likes: 0,
-		comments: []
-	}
+  // Define our post object
+  let newPost = {
+    name: req.body.name,
+    content: req.body.content,
+    timestamp: Date.now(),
+    likes: Number(req.body.likes) || 0,
+    comments: [],
+  };
 
-	// Fetch the posts collection
-	let collection = await db.collection("posts");
+  // Fetch the posts collection
+  let collection = await db.collection("posts");
 
-	// Insert our post into the posts collection
-	await collection.insertOne(newPost)
-	res.send(200)
+  // Insert our post into the posts collection
+  await collection.insertOne(newPost);
+  res.send(200);
+});
+
+app.delete("/", async (req, res) {
+
+})
+
+app.put("/", async (req, res) {
+	
 })
 
 app.listen(port, () => {
-	console.log("Express running on port " + port)
-})
+  console.log("Express running on port " + port);
+});
